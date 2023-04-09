ab2dc4698f8a1a2beba6e5d30abb58b5bc9223b3
jackiecrazy <jackiecrazyhi@gmail.com>
2023-04-08 23:44:48 -0700

    I am a dwarf and I'm coding a tag taggy taggy tag

--- src/main/java/jackiecrazy/cloakanddagger/utils/CombatUtils.java
+++ src/main/java/jackiecrazy/cloakanddagger/utils/CombatUtils.java
@@ -7,6 +7,8 @@ import jackiecrazy.cloakanddagger.config.GeneralConfig;
 import jackiecrazy.footwork.api.CombatDamageSource;
 import jackiecrazy.footwork.api.FootworkAttributes;
 import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
@@ -24,7 +26,7 @@ import javax.annotation.Nullable;
 import java.util.*;
 
 public class CombatUtils {
-    public static HashMap<Item, AttributeModifier> armorStats = new HashMap<>();
+    public static HashMap<TagKey<Item>, StabInfo> archetypes = new HashMap<>();
     private static StabInfo DEFAULTMELEE = new StabInfo(1, 1);
     private static HashMap<Item, StabInfo> combatList = new HashMap<>();
 
@@ -36,9 +38,22 @@ public class CombatUtils {
             JsonObject file = value.getAsJsonObject();
             file.entrySet().forEach(entry -> {
                 final String name = entry.getKey();
+                if (name.startsWith("#")) {//register tags separately
+                    try {
+                        JsonObject obj = entry.getValue().getAsJsonObject();
+                        StabInfo put = new StabInfo(GeneralConfig.distract, GeneralConfig.unaware);
+                        if (obj.has("distracted")) put.distractDamageBonus = obj.get("distracted").getAsDouble();
+                        if (obj.has("unaware")) put.unawareDamageBonus = obj.get("unaware").getAsDouble();
+                        archetypes.put(ItemTags.create(new ResourceLocation(CloakAndDagger.MODID, name.substring(1))), put);
+                    } catch (Exception x) {
+                        CloakAndDagger.LOGGER.error("malformed json under " + name + "!");
+                        x.printStackTrace();
+                    }
+                    return;
+                }
                 ResourceLocation i = new ResourceLocation(name);
                 Item item = ForgeRegistries.ITEMS.getValue(i);
-                if (item == null||item== Items.AIR) {
+                if (item == null || item == Items.AIR) {
                     return;
                 }
                 try {
@@ -81,18 +96,23 @@ public class CombatUtils {
         return !s.isExplosion() && !s.isFire() && !s.isMagic() && !s.isBypassArmor();
     }
 
+    private static StabInfo lookupStats(ItemStack is) {
+        if (combatList.containsKey(is.getItem())) return combatList.get(is.getItem());
+        for (TagKey<Item> tag : archetypes.keySet()) {
+            if (is.is(tag))
+                return archetypes.get(tag);
+        }
+        return DEFAULTMELEE;
+    }
+
     public static double getDamageMultiplier(StealthOverride.Awareness a, ItemStack is) {
         if (is == null) return 1;
-        StabInfo ci = combatList.getOrDefault(is.getItem(), DEFAULTMELEE);
-        switch (a) {
-            case DISTRACTED:
-                return ci.distractDamageBonus;
-            case UNAWARE:
-                return ci.unawareDamageBonus;
-            default:
-                return 1;
-
-        }
+        StabInfo ci = lookupStats(is);
+        return switch (a) {
+            case DISTRACTED -> ci.distractDamageBonus;
+            case UNAWARE -> ci.unawareDamageBonus;
+            default -> 1;
+        };
     }
 
     public static boolean isCrit(CriticalHitEvent e) {
@@ -100,7 +120,7 @@ public class CombatUtils {
     }
 
     public static boolean isWeapon(ItemStack itemStack) {
-        return combatList.containsKey(itemStack.getItem());
+        return lookupStats(itemStack) != DEFAULTMELEE;
     }
 
     private static class StabInfo {
